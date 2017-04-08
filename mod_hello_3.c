a2ce0d3efa1d12b7a62dfa946743c22ba18e640e
Bo Feng <bobbyfengb@gmail.com>
2017-04-08 22:05:05 +0800

    4MB char device driver question 2

--- mod_hello_3.c
+++ mod_hello_3.c
@@ -16,17 +16,40 @@ int onebyte_release(struct inode *inode, struct file *filep);
 ssize_t onebyte_read(struct file * filep, char *buf, size_t count, loff_t *f_pos);
 ssize_t onebyte_write(struct file * filep, const char *buf, size_t count, loff_t *f_pos);
 static void onebyte_exit(void);
+loff_t onebyte_lseek (struct file * filep, loff_t f_pos, int whence);
 
 /* definition of file_operation structure */
 struct file_operations onebyte_fops = {
+	llseek:  onebyte_lseek,
 	read:	onebyte_read,
 	write:	onebyte_write,
 	open:	onebyte_open,
 	release: onebyte_release
 };
 
-char *onebyte_data = NULL;
+char *fourmegabyte_data = NULL;
 
+loff_t onebyte_lseek (struct file * filep, loff_t off, int whence)
+{
+	loff_t newpos;
+	switch(whence) {
+		case 0: /* SEEK_SET */
+			newpos = off;
+			break;
+		case 1: /* SEEK_CUR */
+			newpos = filep->f_pos + off;
+			break;
+		case 2: /* SEEK_CUR */
+			newpos = 4*1024*1024 + off;
+			break;
+
+		default: /* can't happen */
+			return -EINVAL;
+	}
+	if (newpos < 0) return -EINVAL;
+	filep->f_pos = newpos;
+	return newpos;
+}
 int onebyte_open(struct inode *inode, struct file *filep)
 {
 	return 0; // always successful
@@ -39,38 +62,31 @@ int onebyte_release(struct inode *inode, struct file *filep)
 
 ssize_t onebyte_read(struct file * filep, char *buf, size_t count, loff_t *f_pos)
 {
-	if (*f_pos == 0) {
-		copy_to_user(buf, onebyte_data, 1);
-		*f_pos += 1;
-		printk(KERN_INFO "DevOne: Sent %c to the user\n", *buf);
-		return 1;
+	printk(KERN_INFO "Read DevOne: Data %s \n", fourmegabyte_data+*f_pos);
+	printk(KERN_INFO "Read DevOne: Data Length %ld \n", count);
+	if (*f_pos <= 4*1024*1024) {
+		if (*f_pos + count > 4*1024*1024) {
+			count = ((4*1024*1024) - *f_pos);
+		}
+		copy_to_user(buf, fourmegabyte_data+*f_pos, count);
+		*f_pos+=count;
+		return count;
 	} else {
 		return 0;
 	}
-/*	int error_count = 0;
-	*buf = 'x';
-	printk(KERN_INFO "DevOne: Data %c \n", filep->private_data);
-	
-	error_count = copy_to_user(buf, onebyte_data, 1);
-	if (error_count==0)
-	{
-		printk(KERN_INFO "DevOne: Sent %d characters to the user\n", 1);
-		return 0;
-	}
-	else {
-		printk(KERN_ERR "DevOne: Failed to send %c chracters to the user\n", *onebyte_data);
-		return -EFAULT;
-	}*/
 }
 
 ssize_t onebyte_write(struct file * filep, const char *buf, size_t count, loff_t *f_pos)
 {
-	/* please complete the function on your own */
-	printk(KERN_INFO "DevOne: Data %c \n", *buf);
-	if (*f_pos == 0) {
-		copy_from_user(onebyte_data, buf, 1);
-		*f_pos+=1;
-		return 1;
+	printk(KERN_INFO "Write DevOne: Current Writing Position: %lld \n", *f_pos);
+	if (*f_pos <= 4*1024*1024) {
+		if (*f_pos + count > 4*1024*1024) {
+			count = ((4*1024*1024) - *f_pos);
+		}
+		copy_from_user(fourmegabyte_data+*f_pos, buf, count);
+		*f_pos+=count;
+		printk(KERN_INFO "%ld has been written to /dev/one.\n", count);
+		return count;
 	}
 	else {
 		return -ENOSPC;
@@ -90,16 +106,18 @@ static int onebyte_init(void)
 	// kmalloc is just like malloc, the second parameter is 
 	// the type of memory to be allocated.
 	// To release the memory allocated by kmalloc, use kfree.
-	onebyte_data = kmalloc(sizeof(char), GFP_KERNEL);
-	if (!onebyte_data) {
+	fourmegabyte_data = kmalloc((4*1024*1024), GFP_KERNEL);
+	if (!fourmegabyte_data) {
 		onebyte_exit();
 		// cannot allocate memory
 		// return no memory error, negative signify a 
 		// failure
 		return -ENOMEM;
 	}
+	memset(fourmegabyte_data, 0, (4*1024*1024));
 	// initialize the value to be X
-	*onebyte_data = 'X';
+	sprintf(fourmegabyte_data, "X");
+//	*fourmegabyte_data = "X\0";
 	printk(KERN_ALERT "This is a onebyte device module\n");
 	return 0;
 }
@@ -107,10 +125,10 @@ static int onebyte_init(void)
 static void onebyte_exit(void)
 {
 	// if the pointer is pointing to something
-	if (onebyte_data) {
+	if (fourmegabyte_data) {
 		// free the memory and assign the pointer to NULL
-		kfree(onebyte_data);
-		onebyte_data = NULL;
+		kfree(fourmegabyte_data);
+		fourmegabyte_data = NULL;
 	}
 	// unregister the device
 	unregister_chrdev(MAJOR_NUMBER, "onebyte");
